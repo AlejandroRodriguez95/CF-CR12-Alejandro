a6bb365a8f0140b32bc6573f255174d44569862a
Alejandro <alejandro.rodriguez95@gmail.com>
2020-11-27 17:43:36 +0100

    finished cr12

---
+++ base for theme.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <!-- Navbar starts -->
+    <!-- (we made the navbar using wordpress) -->
+    <!-- Navbar ends -->
+
+
+    <div class="container">
+
+        <h1 class="pt-3 text-center mb-0"><i>Website Title</i></h1>
+        <h5 class="text-center pb-3"><i><b>slogan</i></b></h5>
+
+        <div class="row">
+
+            <div class="col-lg-4 col-sm-12 col-md-6">
+                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
+                <h4 class="text-center"><a href="#">Title</a></h4>
+                <p class="text-center"><i>content</i></p>
+                <p><i>Date</i></p>
+                <p>Posted by: Author</p>
+            </div>
+
+            <div class="col-lg-4 col-sm-12 col-md-6">
+                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
+                <h4 class="text-center"><a href="#">Title</a></h4>
+                <p class="text-center"><i>content</i></p>
+                <p><i>Date</i></p>
+                <p>Posted by: Author</p>
+            </div>
+            <div class="col-lg-4 col-sm-12 col-md-6">
+                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
+                <h4 class="text-center"><a href="#">Title</a></h4>
+                <p class="text-center"><i>content</i></p>
+                <p><i>Date</i></p>
+                <p>Posted by: Author</p>
+            </div>
+            <div class="col-lg-4 col-sm-12 col-md-6">
+                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
+                <h4 class="text-center"><a href="#">Title</a></h4>
+                <p class="text-center"><i>content</i></p>
+                <p><i>Date</i></p>
+                <p>Posted by: Author</p>
+            </div>
+            <div class="col-lg-4 col-sm-12 col-md-6">
+                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
+                <h4 class="text-center"><a href="#">Title</a></h4>
+                <p class="text-center"><i>content</i></p>
+                <p><i>Date</i></p>
+                <p>Posted by: Author</p>
+            </div>
+            <div class="col-lg-4 col-sm-12 col-md-6">
+                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
+                <h4 class="text-center"><a href="#">Title</a></h4>
+                <p class="text-center"><i>content</i></p>
+                <p><i>Date</i></p>
+                <p>Posted by: Author</p>
+            </div>
+
+        </div>
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ readme.txt
@@ -0,0 +1,5 @@
+I started working on a single php file at first, and from there went on to make the wordpress theme.
+For this reason, the original file doesn't have everything that the theme has, but since I read on the assignment
+that this file is required, I tried to make the original file again. The file is called base for theme.php
+
+Although the theme is quite simplistic, everything should be working as intended!
