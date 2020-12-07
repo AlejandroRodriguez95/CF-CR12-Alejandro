bbafd8f920ff3fc6a284948454dbd03f8bab2873
Alejandro <alejandro.rodriguez95@gmail.com>
2020-12-07 05:41:56 +0100

    added what i forgot

--- base for theme.php
+++ base for theme.php
@@ -7,78 +7,208 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
+    <link rel='stylesheet' href='style.css'>
 </head>
 
 <body>
 
-    <!-- Navbar starts -->
-    <!-- (we made the navbar using wordpress) -->
-    <!-- Navbar ends -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
 
 
     <div class="container">
+    <h1 class="pt-3 text-center mb-0 title"><i>Travel with style</i></h1>
+    <br>
+    <h5 class="text-center pb-3"><i><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit</i></b></h5>
+</div>
 
-        <h1 class="pt-3 text-center mb-0"><i>Website Title</i></h1>
-        <h5 class="text-center pb-3"><i><b>slogan</i></b></h5>
 
+    <div class=container>
         <div class="row">
-
-            <div class="col-lg-4 col-sm-12 col-md-6">
-                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
-                <h4 class="text-center"><a href="#">Title</a></h4>
-                <p class="text-center"><i>content</i></p>
-                <p><i>Date</i></p>
-                <p>Posted by: Author</p>
+            <div class="card col-lg-4 p-2 mb-3 ">
+                <img class='card-img-top' src='placeholder.png'>
+                <div class="card-body">
+                    <h4 class="text-center"><a href="Title"></a></h4>
+                    <p class="text-center"><i>Lorem ipsum dolor sit amet.</i></p>
+                    <hr>
+                    <p><i>1999/99/99</i></p>
+                    <hr>
+                    <p><b>Posted by:</b>Poster</p>
+                </div>
             </div>
 
-            <div class="col-lg-4 col-sm-12 col-md-6">
-                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
-                <h4 class="text-center"><a href="#">Title</a></h4>
-                <p class="text-center"><i>content</i></p>
-                <p><i>Date</i></p>
-                <p>Posted by: Author</p>
-            </div>
-            <div class="col-lg-4 col-sm-12 col-md-6">
-                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
-                <h4 class="text-center"><a href="#">Title</a></h4>
-                <p class="text-center"><i>content</i></p>
-                <p><i>Date</i></p>
-                <p>Posted by: Author</p>
-            </div>
-            <div class="col-lg-4 col-sm-12 col-md-6">
-                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
-                <h4 class="text-center"><a href="#">Title</a></h4>
-                <p class="text-center"><i>content</i></p>
-                <p><i>Date</i></p>
-                <p>Posted by: Author</p>
-            </div>
-            <div class="col-lg-4 col-sm-12 col-md-6">
-                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
-                <h4 class="text-center"><a href="#">Title</a></h4>
-                <p class="text-center"><i>content</i></p>
-                <p><i>Date</i></p>
-                <p>Posted by: Author</p>
-            </div>
-            <div class="col-lg-4 col-sm-12 col-md-6">
-                <img class="rounded" width="100%" src='https://designshack.net/wp-content/uploads/placeholder-image.png'>
-                <h4 class="text-center"><a href="#">Title</a></h4>
-                <p class="text-center"><i>content</i></p>
-                <p><i>Date</i></p>
-                <p>Posted by: Author</p>
+            <div class="card col-lg-4 p-2 mb-3 ">
+                <img class='card-img-top' src='placeholder.png'>
+                <div class="card-body">
+                    <h4 class="text-center"><a href="Title"></a></h4>
+                    <p class="text-center"><i>Lorem ipsum dolor sit amet.</i></p>
+                    <hr>
+                    <p><i>1999/99/99</i></p>
+                    <hr>
+                    <p><b>Posted by:</b>Poster</p>
+                </div>
             </div>
 
+            <div class="card col-lg-4 p-2 mb-3 ">
+                <img class='card-img-top' src='placeholder.png'>
+                <div class="card-body">
+                    <h4 class="text-center"><a href="Title"></a></h4>
+                    <p class="text-center"><i>Lorem ipsum dolor sit amet.</i></p>
+                    <hr>
+                    <p><i>1999/99/99</i></p>
+                    <hr>
+                    <p><b>Posted by:</b>Poster</p>
+                </div>
+            </div>
+            <div class="card col-lg-4 p-2 mb-3 ">
+                <img class='card-img-top' src='placeholder.png'>
+                <div class="card-body">
+                    <h4 class="text-center"><a href="Title"></a></h4>
+                    <p class="text-center"><i>Lorem ipsum dolor sit amet.</i></p>
+                    <hr>
+                    <p><i>1999/99/99</i></p>
+                    <hr>
+                    <p><b>Posted by:</b>Poster</p>
+                </div>
+            </div>
+            <div class="card col-lg-4 p-2 mb-3 ">
+                <img class='card-img-top' src='placeholder.png'>
+                <div class="card-body">
+                    <h4 class="text-center"><a href="Title"></a></h4>
+                    <p class="text-center"><i>Lorem ipsum dolor sit amet.</i></p>
+                    <hr>
+                    <p><i>1999/99/99</i></p>
+                    <hr>
+                    <p><b>Posted by:</b>Poster</p>
+                </div>
+            </div>
+            <div class="card col-lg-4 p-2 mb-3 ">
+                <img class='card-img-top' src='placeholder.png'>
+                <div class="card-body">
+                    <h4 class="text-center"><a href="Title"></a></h4>
+                    <p class="text-center"><i>Lorem ipsum dolor sit amet.</i></p>
+                    <hr>
+                    <p><i>1999/99/99</i></p>
+                    <hr>
+                    <p><b>Posted by:</b>Poster</p>
+                </div>
+            </div>
+        </div>
         </div>
 
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">Footer Content</h5>
+        <p>Here you can use rows and columns to organize your footer content.</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase">Links</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Link 1</a>
+          </li>
+          <li>
+            <a href="#!">Link 2</a>
+          </li>
+          <li>
+            <a href="#!">Link 3</a>
+          </li>
+          <li>
+            <a href="#!">Link 4</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
     </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <b><a href="https://www.codefactory.com/"> www.codefactory.com</a></b>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
 
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
-    </script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
